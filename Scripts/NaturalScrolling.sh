a9e9f2374e333d0076bb881b2d73686e8bf40cde
tdl <troyd_000@ubuntu.(none)>
2013-03-19 15:29:06 -0700

    first commit.  touchmods script

---
+++ Scripts/NaturalScrolling.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xinput set-prop 12 288 106 -106
+nautilus -q
+nautilus -n &
---
+++ Scripts/touchmods.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+synclient HorizScrollDelta=-79
+synclient MaxSpeed=7
+synclient AccelFactor=.125
+synclient HorizScrollDelta=-125
+synclient VertScrollDelta=35
+#synclient HorizHysteresis=10
+#synclient VertHysteresis=10
+synclient CoastingSpeed=10 #allows momentum scrolling
+synclient CoastingFriction=25 #how fast the momentum fades
+synclient PalmDetect=1
+synclient MaxTapTime=250
