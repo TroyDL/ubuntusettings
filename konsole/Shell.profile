ae451b49731479e18f1cd4cd3cf50e52c2f722ca
tdl <tdl@tdl-700Z7C.(none)>
2013-04-12 21:17:50 -0700

    added to postinstallscript.sh and added config files and folders

---
+++ konsole/Shell.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Solarized
+Font=DejaVu Sans Mono,10,-1,5,75,0,0,0,0,0
+
+[General]
+Environment=TERM=xterm-256color
+Name=Shell
+Parent=FALLBACK/
--- scripts/postfreshinstall.sh
+++ scripts/postfreshinstall.sh
@@ -1,21 +1,24 @@
 #install cinnamon
 sudo add-apt-repository -y ppa:gwendal-lebihan-dev/cinnamon-stable
 sudo apt-get update
+sudo apt-get install -y vim
 sudo apt-get install -y cinnamon
-sudo apt-get install -y git
 sudo apt-get install -y synapse
 sudo apt-get install -y konsole
 sudo apt-get install -y build-dep touchegg
 
 cd ~/
-git clone https://github.com/TroyDL/ubuntusettings.git
 git clone https://github.com/TroyDL/termprefs.git
+git clone https://github.com/TroyDL/ubuntusettings.git
 mv termprefs .matilde
 cp .matilde/.vimrc ~/
 cp .matilde/.bashrc ~/
 cp .matilde/.bash_profile ~/
-
-
-
+cp -r .matilde/.vim ~/
+cp ~/ubuntusettings/konsole/* ~/.kde/share/apps/konsole/
+cd ~/ubuntusettings/scripts/
+./touchmods.sh
+cd ~
+cp ~/ubuntusettings/autostart/* ~/.config/autostart/
 
 
