92b6d019718f8ffd80bf360c61f1431c754e169b
Troy Lonie <troydl@mail.fresnostate.edu>
2013-05-03 21:09:46 -0700

    fixed vimrc

--- scripts/touchmods.sh
+++ scripts/touchmods.sh
@@ -2,12 +2,12 @@
 synclient MaxSpeed=7
 synclient AccelFactor=.125
 synclient HorizScrollDelta=-125
-synclient VertScrollDelta=-79
+#synclient VertScrollDelta=-79
 #synclient HorizHysteresis=10
 #synclient VertHysteresis=10
 synclient CoastingSpeed=10 #allows momentum scrolling
 synclient CoastingFriction=25 #how fast the momentum fades
-synclient PalmDetect=1
+synclient PalmDetect=0
 #synclient MaxTapTime=250
 synclient HorizTwoFingerScroll=1
 
