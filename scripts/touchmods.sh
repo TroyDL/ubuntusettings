8a8b0c45ac4a2770d2c2176e34efa9886f66c5fc
TroyDL <troydl@gmail.com>
2013-11-25 13:30:53 -0800

    Slightly modified touchmods.sh for two finger scrolling

--- scripts/touchmods.sh
+++ scripts/touchmods.sh
@@ -2,11 +2,11 @@
 synclient MaxSpeed=7
 synclient AccelFactor=.125
 synclient HorizScrollDelta=-125
-synclient VertScrollDelta=-79
+synclient VertScrollDelta=-60
 #synclient HorizHysteresis=10
 #synclient VertHysteresis=10
-synclient CoastingSpeed=10 #allows momentum scrolling
-synclient CoastingFriction=25 #how fast the momentum fades
+synclient CoastingSpeed=1 #allows momentum scrolling
+synclient CoastingFriction=2 #how fast the momentum fades
 synclient PalmDetect=0
 #synclient MaxTapTime=250
 synclient HorizTwoFingerScroll=1
